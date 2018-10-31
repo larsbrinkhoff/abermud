ea371c27c5839f2f0f0ada1e5243f326249242be
Lars Brinkhoff <lars@nocrew.org>
2018-10-31 11:18:49 +0100

    old

---
+++ abermud2/MAKEWORL.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+        World Creator
+*/
+ 
+ 
+void main()
+{
+        FILE *a;
+        long x[64];
+        long b;
+        a=fopen("/usr/tmp/-[iy7]|AM","w");
+        x[0]=1;
+        x[1]=1;
+        sec_write(a,x,0,64);
+        b=0;
+        while(b<600)
+        {
+                sec_write(a,x,b,64);
+                x[0]=0;
+                b++;
+        }
+        fclose(a);
+}
